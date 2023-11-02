963e1e8e0ba3644831d27b57b3e37377f111448c
atharvaawatade <97244560+atharvaawatade@users.noreply.github.com>
2023-11-02 21:02:05 +0530

    Add files via upload

---
+++ Day-1/Basic introduction.py
@@ -0,0 +1,44 @@
+#50-days-python-challenge
+
+'''
+Today I learned about the basic introduction of the python.
+
+what are the modules
+ -Built-in Modules
+ - External Modules
+'''
+# example :
+
+import playsound #this module used for playing sound using ai voice for the result
+
+'''
+I also Learned about the comments in python
+# => used for single line comment
+'''''' => used for multiline comments
+'''
+
+#Escape sequence character 
+
+'''
+Those characters that cannot be directly used in string, we use an escape sequence character
+
+it is backslash \ followed by character we insert
+'''
+
+#example
+
+print('hello  I am\' "student" \nwe are here to lern ')
+#output: hello  I am' "student" 
+# we are here to lern
+
+print("hello", 4,5,6, sep=("&"), end=("nextline \n"))
+print("stop")
+
+#output: hello&4&5&6nextline
+# stop
+
+'''
+sep => used to separate the different output by character we want
+end=> used to addany element at the end
+\n => used to break the line
+'''
