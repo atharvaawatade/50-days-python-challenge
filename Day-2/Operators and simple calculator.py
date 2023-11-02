2d21cf8cc776f1cc758bc49c3a992274057de4dd
atharvaawatade <97244560+atharvaawatade@users.noreply.github.com>
2023-11-02 21:39:06 +0530

    Add files via upload

---
+++ Day-2/Operators and simple calculator.py
@@ -0,0 +1,37 @@
+## Operators
+
+'''
++: addition, -: substraction, *: multiplication, / : divide, % : modulo, //: float division, ** : Exponential
+'''
+
+#example
+
+a = 2
+b = 5
+
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+h = a//b
+i = a**b
+
+print(c,d,e,f,g,h,i)
+#output : 7 -3 10 0.4 2 0 32
+
+## Calculator
+
+j = input("input first number: ")
+k = input("input first number: ")
+
+print(j + k)
+print(j - k)
+print(j * k)
+print(j / k)
+print(j % k)
+print(j // k)
+print(j ** k)
+
+# output: if we take input as 4, 7 then : 7 -3 10 0.4 2 0 32
+
